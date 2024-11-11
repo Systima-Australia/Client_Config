46915035fe4d72243f9577dd82f0802e6b2905ab
Tully <tjagoe@systima.com.au>
2024-11-11 14:21:16 +1100

    Cleanup

--- systimaassets/download_assets.bash
+++ systimaassets/download_assets.bash
@@ -1,44 +1,25 @@
 #!/bin/bash
 
 # MARK: ----------------- Install XCode CLT -----------------
-xcodeCLT() {
-    #!/bin/bash
-    echo -e "\n----XCode CLT install START"
 
-    xcodePath="/Library/Developer/CommandLineTools"
-    xcodeVersion="$(softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
-    echo "${xcodeVersion}"
+# Install Xcode Command Line Tools if not installed
+xcodePath="/Library/Developer/CommandLineTools"
+xcodeVersion="$(softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
 
-    # Command to install Xcode CLT
-    installXcode() {
-        echo "Installing Xcode CLT"
-        touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-        echo "Installing Xcode Command Line Tools version: ${xcodeVersion}"
-        sudo softwareupdate -i --verbose "${xcodeVersion}"
-        sudo rm -vf "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-        xcode-select -s "${xcodePath}"
-    }
+# Command to install Xcode CLT
+echo "Checking Xcode CLT..."
+[[ "${xcodeVersion}" == *"No new software available."* ]] && {
+    echo "Installing Xcode Command Line Tools version: ${xcodeVersion}"
+    touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+    sudo softwareupdate -i --verbose "${xcodeVersion}"
+    sudo rm -vf "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+    xcode-select -s "${xcodePath}"
+}
 
-    # Install Xcode CLT if not installed
-    [[ ! -d "${xcodePath}" ]] && {
-        echo "Xcode CLT not installed"
-        installXcode
-    } || echo "Confirmed Xcode CLT installed"
-
-    # Set Xcode CLT path if not set, or incorrectly set to App path
-    [[ $(xcode-select -p) != "${xcodePath}" ]] && {
-        echo "Setting XCode CLT path to ${xcodePath}"
-        xcode-select -s "${xcodePath}"
-    }
-
-    # Update Xcode CLT if available
-    echo "Checking for Xcode CLT updates..."
-    [[ "${xcodeVersion}" == *"No new software available."* && -d "${xcodePath}" ]] && {
-        echo "Xcode CLT update available"
-        installXcode
-    } || echo "No update available"
-
-    echo -e "----XCode CLT install END\n"
+# Set Xcode CLT path if not set, or incorrectly set to App path
+[[ $(xcode-select -p) != "${xcodePath}" ]] && {
+    echo "Setting XCode CLT path to ${xcodePath}"
+    xcode-select -s "${xcodePath}"
 }
 
 # ----------------- Download or update Systima assets from GitHub -----------------
@@ -46,9 +27,6 @@ downloadAssets() {
     # App to update
     gitRepo="${1}"
     localDir="/Library/Application Support/Systima/${gitRepo}"
-
-    # Install Xcode Command Line Tools if not installed
-    xcodeCLT
 
     # Download or update assets
     echo -e "\n    ----${gitRepo} START"
