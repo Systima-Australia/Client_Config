d5893c5dea9b2ab0a1dc912e5e781f8402a650a4
Tully <tjagoe@systima.com.au>
2024-11-14 15:57:18 +1100

    better xcode / git checks

--- systimaassets/download_assets.bash
+++ systimaassets/download_assets.bash
@@ -1,26 +1,42 @@
 #!/bin/bash
 echo -e "\n    ----download_assets START"
 
-# MARK: ----------------- Install XCode CLT -----------------
-
-# Install Xcode Command Line Tools if not installed
-xcodePath="/Library/Developer/CommandLineTools"
-
-# Command to install Xcode CLT
-echo "Checking Xcode CLT..."
-xcodeVersion="$(softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
-[[ "${xcodeVersion}" == *"No new software available."* ]] && {
-    echo "Installing Xcode Command Line Tools version: ${xcodeVersion}"
+xcodeCLT() {
+    # MARK: ----------------- Install XCode CLT -----------------
+    # Install Xcode Command Line Tools if not installed
+    xcodePath="/Library/Developer/CommandLineTools"
+    # Command to install Xcode CLT
+    echo "Checking Xcode CLT..."
     touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-    sudo softwareupdate -i --verbose "${xcodeVersion}"
-    sudo rm -vf "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
-    xcode-select -s "${xcodePath}"
+    xcodeVersion="$(softwareupdate -l | grep -B 1 "Command Line Tools" | awk -F"*" '/^ *\*/ {print $2}' | sed -e 's/^ *Label: //' -e 's/^ *//' | sort -V | tail -n1)"
+    sudo mv "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress" "/tmp/.${xcodeVersion}"
+    [[ "${xcodeVersion}" != *"No new software available."* ]] && {
+        echo -e "Installing Xcode Command Line Tools version:\n${xcodeVersion}"
+        #touch "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+        sudo softwareupdate -i --verbose "${xcodeVersion}"
+
+        echo "Waiting for Xcode Command Line Tools installation to complete..."
+        updateCheck=$(pgrep -x "softwareupdate")
+        while [[ "$updateCheck" != "" ]]; do
+            sleep 10
+            updateCheck=$(pgrep -x "softwareupdate")
+        done
+        echo "Xcode Command Line Tools installation complete"
+
+        sudo rm -vf "/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+        xcode-select -s "${xcodePath}"
+    }
+
+    # Set Xcode CLT path if not set, or incorrectly set to App path
+    [[ $(xcode-select -p) != "${xcodePath}" ]] && {
+        echo "Setting XCode CLT path to ${xcodePath}"
+        xcode-select -s "${xcodePath}"
+    }
 }
 
-# Set Xcode CLT path if not set, or incorrectly set to App path
-[[ $(xcode-select -p) != "${xcodePath}" ]] && {
-    echo "Setting XCode CLT path to ${xcodePath}"
-    xcode-select -s "${xcodePath}"
+[[ $(! command -v git &> /dev/null) ]] && {
+    echo "Git not detected, checking xcode CLT..."
+    xcodeCLT
 }
 
 # ----------------- Download or update Systima assets from GitHub -----------------
